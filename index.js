8e4eb34e58d2c181447621b2b69043f0afac9492
Teemu Sarapisto <tsarpf@gmail.com>
2017-05-13 23:25:06 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+const Promise = require('bluebird')
+const conf = require('./settings.json')
+
+const instance = axios.create({
+  baseURL: 'https://api.cloudflare.com/client/v4',
+  headers: {
+    'X-Auth-Email': conf.email,
+    'X-Auth-Key': conf.authKey,
+    'Content-Type': 'application/json'
+  }
+})
+
+let zoneId
+instance.get('/zones', {name: conf.domain})
+  .then(e => {
+    const result = e.data.result.filter(z => z.name === conf.domain)
+
+    if (result.length > 1) {
+      console.log('warning! more than one zone was found with the given domain, only updating first one')
+    } else if (result.length === 0) {
+      throw 'no zone found with given domain'
+    }
+
+    zoneId = result[0].id
+    return instance.get(`/zones/${zoneId}/dns_records`)
+  })
+  .then(e =>
+        Promise.all(
+          e.data.result
+            .filter(item => item.type === 'A')
+            .map(item => instance.put(`/zones/${zoneId}/dns_records/${item.id}`, {
+              type: item.type,
+              name: item.name,
+              content: conf.newAddress
+            })))
+       )
+  .then(() => {
+    console.log('done!')
+  })
+  .catch(e => console.log(e))
